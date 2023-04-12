bcfc8cfd974467fab0beef7482331fcd8c10136b
BaHeK.G <44212827+psixona@users.noreply.github.com>
2023-04-12 22:58:40 +0300

    My hw / module 10 / task 1-3 / code refactoring

---
+++ src/test/java/org/itstep/module10/hw1/ValidatorTest.java
@@ -0,0 +1,45 @@
+package org.itstep.module10.hw1;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ValidatorTest {
+  private Validator validator;
+
+  @BeforeEach
+  void setUp() {
+    validator = new Validator();
+  }
+
+  @Test
+  void isValid() {
+    assertFalse(validator.isValid("123 456 7890"));
+  }
+  @Test
+  void isValid2() {
+    assertFalse(validator.isValid("123456 7890"));
+  }
+  @Test
+  void isValid3() {
+    assertFalse(validator.isValid("1234567890"));
+  }
+  @Test
+  void isValid4() {
+    assertFalse(validator.isValid("(123) (456)-7890"));
+  }
+  @Test
+  void isValid5() {
+    assertFalse(validator.isValid("(123)-456-7890"));
+  }
+  @Test
+  void isValid6() {
+    assertTrue(validator.isValid("(123) 456-7890"));
+  }
+  @Test
+  void isValid7() {
+    assertTrue(validator.isValid("987-123-4567"));
+  }
+}
