d834217b04584bf59bc0dbe9b0aa9b22e21fce7b
BaHeK.G <44212827+psixona@users.noreply.github.com>
2023-04-20 19:29:53 +0300

    My hw / module 11 / task 3 / change method
    Took 52 seconds

--- src/main/java/org/itstep/module11/hw3/Main.java
+++ src/main/java/org/itstep/module11/hw3/Main.java
@@ -4,9 +4,9 @@ import java.util.List;
 
 public class Main {
   public static void main(String[] args) {
-    String[] arrayForSort = {"1, 2, 0", "4, 5"};
+    String[] arrayForSort = {"1, 2, 0", "4, 5", "10, 15, 22"};
     ArraySorter arraySorter = new ArraySorter();
     List<Integer> numberList = arraySorter.sortArray(arrayForSort);
-    System.out.println(numberList.stream().toList());
+    System.out.println(numberList);
   }
 }
