3103f7a86e5eb5ef5b74d31903a2cd687f2c1140
BaHeK.G <44212827+psixona@users.noreply.github.com>
2023-04-20 19:27:54 +0300

    My hw / module 11 / task 2 / change method
    Took 12 seconds

--- src/main/java/org/itstep/module11/hw2/Main.java
+++ src/main/java/org/itstep/module11/hw2/Main.java
@@ -1,8 +1,15 @@
 package org.itstep.module11.hw2;
 
+import java.util.List;
+
 public class Main {
+  private static final List<String> NAMES = List.of("Ivan0", "Ivan1", "Ivan2", "Ivan3", "Ivan4", "Ivan5", "Anton", "Michail");
+
   public static void main(String[] args) {
+
     NameSorter nameSorter = new NameSorter();
-    nameSorter.reader("Anton, Ivan0, Ivan1, Ivan2, Ivan3, Ivan4, Ivan5, Michail");
+
+    List<String> sortedListNames = nameSorter.sorter(NAMES);
+    System.out.println(sortedListNames);
   }
 }
--- src/main/java/org/itstep/module11/hw2/NameSorter.java
+++ src/main/java/org/itstep/module11/hw2/NameSorter.java
@@ -1,21 +1,13 @@
 package org.itstep.module11.hw2;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class NameSorter {
 
-  public void reader(String listOfName) {
-    Map<Integer, String> mapOfName = new HashMap<>();
-    String[] arrayOfNames = listOfName.split(",");
-    int i = 0;
-    for (String element : arrayOfNames) {
-      mapOfName.put(i, element.trim());
-      i++;
-    }
-    mapOfName.entrySet().stream()
-      .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
-      .forEach(v -> System.out.printf("%d. %s, ", v.getKey(), v.getValue()));
+  public List<String> sorter(List<String> listOfName) {
+    return listOfName.stream()
+      .map(String::toUpperCase)
+      .sorted(Comparator.reverseOrder())
+      .toList();
   }
 }
