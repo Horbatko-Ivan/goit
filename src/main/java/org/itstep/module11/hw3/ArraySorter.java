85ee72eee6d5de6a542b4a39f4d7a1c8c375e9e2
BaHeK.G <44212827+psixona@users.noreply.github.com>
2023-04-20 19:29:00 +0300

    My hw / module 11 / task 3 / change method
    Took 8 seconds

--- src/main/java/org/itstep/module11/hw3/ArraySorter.java
+++ src/main/java/org/itstep/module11/hw3/ArraySorter.java
@@ -1,23 +1,19 @@
 package org.itstep.module11.hw3;
 
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.LinkedList;
+import java.util.Collection;
 import java.util.List;
 
 public class ArraySorter {
-  private final List<Integer> result = new LinkedList<>();
 
-  public void sortArray(String[] dataArray) {
-    Arrays.stream(dataArray)
-      .forEach(s -> Arrays.stream(s.trim().split(","))
-        .forEach(el -> result.add(Integer.valueOf(el.trim()))));
-    Collections.sort(result);
-    StringBuilder sb = new StringBuilder();
-    for (int i = 0; i < result.size() - 1; i++) {
-      sb.append(result.get(i)).append(", ");
-    }
-    sb.append(result.get(result.size() - 1));
-    System.out.println(sb);
+  public List<Integer> sortArray(String[] dataArray) {
+    return Arrays.stream(dataArray)
+      .map(s -> Arrays.stream(s.trim().split(","))
+        .mapToInt(el -> Integer.parseInt(el.trim()))
+        .boxed()
+        .toList())
+      .flatMap(Collection::stream)
+      .sorted()
+      .toList();
   }
 }
